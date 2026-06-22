43d273c0046e173380ea0a19ab49934560f8360b
Ryan <39524470+ryankahil@users.noreply.github.com>
2026-06-22 14:03:11 -0400

    KAN-4: Adding parameters and code for public/private subnets

--- cloud-engineering/ebusa/vpc/variables.tf
+++ cloud-engineering/ebusa/vpc/variables.tf
@@ -1,3 +1,5 @@
+##### VPC Variables #####
+
 variable "cidr_block" {
   description = "CIDR Block for VPC"
   type        = string
@@ -35,3 +37,75 @@ variable "VPC-Name" {
   description = "VPC Name"
   type        = string
 }
+
+##### Public Subnet Variables #####
+
+variable "public_subnet1_cidr" {
+  description = "Public Subnet 1 CIDR "
+  type        = string
+}
+
+variable "public_subnet2_cidr" {
+  description = "Public Subnet 2 CIDR "
+  type        = string
+}
+
+variable "az_public_subnet1" {
+  description = "Availability Zone for Public Subnet 1"
+  type        = string
+}
+
+variable "az_public_subnet2" {
+  description = "Availability Zone for Public Subnet 2"
+  type        = string
+}
+
+variable "public_subnet1_name" {
+  desription = "Public Subnet1 Name"
+  type       = string
+}
+
+variable "public_subnet2_name" {
+  desription = "Public Subnet2 Name"
+  type       = string
+}
+
+##### Private Subnet Variables #####
+
+variable "private_subnet1_cidr" {
+  description = "Private Subnet1 CIDR"
+  type        = string
+  default     = "us-east-2a"
+}
+
+variable "private_subnet2_cidr" {
+  description = "Private Subnet2 CIDR"
+  type        = string
+  default     = "us-east-2b"
+}
+
+variable "az_private_subnet1" {
+  description = "Availability Zone for Private Subnet 1"
+  type        = string
+  default     = "us-east-2a"
+}
+
+variable "az_private_subnet2" {
+  description = "Availability Zone for Private Subnet 2"
+  type        = string
+  default     = "us-east-2b"
+}
+
+variable "private_subnet1_name" {
+  desription = "Private Subnet1 Name"
+  type       = string
+}
+
+variable "private_subnet2_name" {
+  desription = "Private Subnet2 Name"
+  type       = string
+}
+
+
+
+
--- cloud-engineering/ebusa/vpc/vpc.tf
+++ cloud-engineering/ebusa/vpc/vpc.tf
@@ -15,17 +15,57 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_vpc" "eb_usa_vpc" {
-  cidr_block            = var.cidr_block
-  enable_dns_support    = var.enable_dns_support
-  enable_dns_hostnames  = var.enable_dns_hostnames
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
 
   tags {
-    Name                = var.VPC-Name
-    Environment         = var.Environment
-    Client              = var.Client
+    Name        = var.VPC-Name
+    Environment = var.Environment
+    Client      = var.Client
   }
 }
 
+resource "aws_subnet" "public_subnet_1" {
+  vpc               = aws_vpc.eb_usa_vpc.id
+  cidr_block        = var.public_subnet_1
+  availability_zone = var.az_public_subnet1
 
+  map_public_ip_on_launch = true
 
+  tags = {
+    Name = var.public_subnet1_name
+  }
+}
 
+resource "aws_subnet" "public_subnet_2" {
+  vpc               = aws_vpc.eb_usa_vpc.id
+  cidr_block        = var.public_subnet_2
+  availability_zone = var.az_public_subnet2
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.public_subnet2_name
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc               = aws_vpc.eb_usa_vpc.id
+  cidr_block        = var.private_subnet1_cidr
+  availability_zone = var.az_private_subnet1
+
+  tags {
+    Name = var.private_subnet1_name
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc               = aws_vpc.eb_usa_vpc.id
+  cidr_block        = var.private_subnet2_cidr
+  availability_zone = var.az_private_subnet2
+
+  tags {
+    Name = var.private_subnet2_name
+  }
+}
