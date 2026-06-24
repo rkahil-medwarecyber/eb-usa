826cbce34ac9df636453680d78cebdb6bfc335f7
Ryan <39524470+ryankahil@users.noreply.github.com>
2026-06-24 15:42:27 -0400

    KAN-4: Adding SFTP variables and code

--- cloud-engineering/ebusa/sftp/sftp.tf
+++ cloud-engineering/ebusa/sftp/sftp.tf
@@ -15,9 +15,19 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_transfer_server" "eb_sftp" {
+  endpoint_type                  = var.endpoint_type
+  domain                         = var.domain
+  identity_provider_type         = var.identity_provider_type
+  security_policy_name           = var.security_policy_name
+  ip_address_type                = var.ip_address_type
+  host_key                       = var.host_key
+  directory_listing_optimization = var.directory_listing_optimization
+
+
   tags = {
-    Name = var.server_name
+    Name        = var.server_name
+    Environment = var.environment
   }
 
-  
+
 }
--- cloud-engineering/ebusa/sftp/variables.tf
+++ cloud-engineering/ebusa/sftp/variables.tf
@@ -1,6 +1,67 @@
 ##### VPC Variables #####
 
-variable "cidr_block" {
-  description = "CIDR Block for VPC"
+variable "server_name" {
+  description = "SFTP Server Name"
   type        = string
 }
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "endpoint_type" {
+  description = "Endpoint Type (VPC, VPC_ENDPOINT, or PUBLIC"
+  type        = string
+  default     = "PUBLIC"
+}
+
+variable "domain" {
+  description = "Storage System for file transfers"
+  type        = string
+  default     = "S3"
+}
+
+variable "protocols" {
+  description = "SFTP"
+  type        = string
+  default     = "SFTP"
+}
+
+variable "identity_provider_type" {
+  description = "Mode of Authentication enabled for service"
+  type        = string
+  default     = "SERVICE_MANAGED"
+}
+
+variable "security_policy_name" {
+  description = "Name of the Security Policy Attached to Server"
+  type        = string
+  default     = "TransferSecurityPolicy-FIPS-2024-01"
+}
+
+variable "sftp_authentication_method" {
+  description = "For SFTP-enabled servers"
+  type        = string
+  default     = "PUBLIC_KEY"
+}
+
+variable "ip_address_type" {
+  description = "IPV4 or Dualstack"
+  type        = string
+  default     = "IPV4"
+}
+
+variable "host_key" {
+  description = "RSA, ECDSA, or ED25519"
+  type        = string
+  default     = "RSA"
+}
+
+variable "directory_listing_optimization" {
+  description = "Whether or not performance for your S3 directories is optimized"
+  type        = string
+  default     = "ENABLED"
+}
+
+
