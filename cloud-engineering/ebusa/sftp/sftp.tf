5ad119a6607a4d63598b642a6ccfa8be512f77e7
Ryan <39524470+ryankahil@users.noreply.github.com>
2026-06-25 14:59:29 -0400

    KAN-4: Fixing another typo

--- cloud-engineering/ebusa/sftp/sftp.tf
+++ cloud-engineering/ebusa/sftp/sftp.tf
@@ -114,7 +114,7 @@ resource "aws_s3_bucket_policy" "eb_policy" {
         }
 
         Action = [
-          "s3:PubObject",
+          "s3:PutObject",
           "s3:GetObject",
           "s3:DeleteObject",
           "s3:GetObjectVersion",
